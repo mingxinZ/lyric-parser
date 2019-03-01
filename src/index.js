8771c74879259cde6d70a25fd46a424cfbeb4a2f
Mingxin.Zheng <mingwangye123@gmail.com>
2019-03-01 10:31:55 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -47,12 +47,8 @@ export default class Lyric {
       if (result) {
         const txt = line.replace(timeExp, '').trim()
         if (txt) {
-          let tirdResult = result[3] || '0' // 数字的 0 查询长度会变为 undefined，感觉不如直接指定 '0'
-          let length = tirdResult.length
-          let __tirdResult = parseInt(tirdResult, 10)
-          __tirdResult = length > 2 ? __tirdResult : __tirdResult * 10
           this.lines.push({
-            time: result[1] * 60 * 1000 + result[2] * 1000 + (result[3] || 0) * 10,
+            time: result[1] * 60 * 1000 + result[2] * 1000 + (result[3] || 0)
             txt
           })
         }
